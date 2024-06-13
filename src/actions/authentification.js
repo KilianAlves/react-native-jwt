a9677c50c2d3f05123c6d734a8badb8bf68a20e8
KilianAlves <alveskilian@gmail.com>
2024-06-13 14:13:26 +0200

    Fin etape 5

--- src/actions/authentification.js
+++ src/actions/authentification.js
@@ -1,9 +1,20 @@
-export async function loginAPI(login, password) {
-  return fetch("http://localhost:8000/auth/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ login: login, password: password }),
-  }).then((response) => response.json());
+export async function loginAPI(data) {
+  if (data.login !== undefined || data.password !== undefined) {
+    console.log("data.login", data.login);
+    return fetch("http://localhost:8000/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ login: data.login, password: data.password }),
+    }).then((response) => response.json());
+  } else {
+    return fetch("http://localhost:8000/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ refreshToken: data.refreshToken }),
+    }).then((response) => response.json());
+  }
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,30 @@
 import { View, TextInput, Button } from "react-native";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { loginAPI } from "../actions/authentification";
 import { useContactContext } from "../hooks/useContactContect";
+import * as SecureStore from "expo-secure-store";
 
 export default function Login({ navigation }) {
   const [login, setLogin] = useState("c");
   const [password, setPassword] = useState("c");
 
-  const { dispatch } = useContactContext();
+  const { state, dispatch } = useContactContext();
+
+  // useEffect pour obtenir le refreshToken et permettre de se co automatiquement
+  useEffect(() => {
+    const getJwtToken = async () => {
+      const jwtToken = await SecureStore.getItemAsync("jwtToken");
+      if (jwtToken) {
+        dispatch({ type: "SET_JWT_TOKEN", payload: jwtToken });
+        navigation.navigate("ContactsList");
+      }
+    };
+    console.log("useEffect");
+    getJwtToken();
+  }, []);
 
   const handleLogin = async () => {
-    const response = await loginAPI(login, password);
+    const response = await loginAPI({ login, password });
     console.log("response");
     console.log("handleLogin =>", response);
 
@@ -18,6 +32,7 @@ export default function Login({ navigation }) {
       console.log("error");
     } else {
       dispatch({ type: "SET_JWT_TOKEN", payload: response.jwt });
+      await SecureStore.setItemAsync("jwtToken", response.jwt);
       navigation.navigate("ContactsList");
     }
   };
