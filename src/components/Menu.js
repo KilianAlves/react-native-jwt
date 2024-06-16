92a57aca188f5c3bec3cee7514038fd9acdf9e55
KilianAlves <alveskilian@gmail.com>
2024-06-16 11:15:01 +0200

    Logout fonctionnelle et fix du secure store

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,14 +3,18 @@ import { useEffect, useState } from "react";
 import { useContactContext } from "../hooks/useContactContect";
 import { getMe } from "../services/api";
 import { style } from "../../App";
+import * as SecureStore from "expo-secure-store";
 
 export default function Menu({ navigation }) {
   const [userInfo, setUserInfo] = useState({});
   const { state, dispatch } = useContactContext();
 
   const LogOut = () => {
-    console.log("LogOut");
+    // Suppression du token
     dispatch({ type: "LOGOUT" });
+    // Suppression du refreshtoken dans le stockage
+    SecureStore.deleteItemAsync("jwtToken");
+    // Redirection vers la page de connexion
     navigation.navigate("Login");
   };
 
